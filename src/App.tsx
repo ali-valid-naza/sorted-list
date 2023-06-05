6d5978d38c9e6cb02448d54d95fcc1a25e82b21d
OlegAnisimov <gramafon@yandex.ru>
2023-06-05 13:04:53 +0300

    flatten data

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import './App.css';
 import axios from "axios";
 
+type Person = any;
+type Location = any;
+
+enum SortingDirection {
+  ASCENDING = "ASCENDING",
+  DESCENDING = "DESCENDING",
+  UNSORTED = "UNSORTED"
+}
 
 const fetchData = () => {
   return axios
@@ -14,6 +22,39 @@ const fetchData = () => {
       console.error(err);
     });
 };
+
+const flattenLocations = (locations: Location[]) => {
+  const location = locations[0];
+  const data = [];
+  for (const { street, coordinates, timezone, ...rest } of locations) {
+    data.push({
+      ...rest,
+      number: street.number,
+      name: street.name,
+      latitude: coordinates.latitude,
+      longitude: coordinates.longitude
+    });
+  }
+
+  const flattenedLocationHeaders = extractObjectKeys(data[0]);
+  return { headers: flattenedLocationHeaders, data };
+};
+
+const extractObjectKeys = (object: any) => {
+  let objectKeys: string[] = [];
+
+  Object.keys(object).forEach((objectKey) => {
+    const value = object[objectKey];
+    if (typeof value !== "object") {
+      objectKeys.push(objectKey);
+    } else {
+      objectKeys = [...objectKeys, ...extractObjectKeys(value)];
+    }
+  });
+
+  return objectKeys;
+};
+
 
 function App() {
   return (
