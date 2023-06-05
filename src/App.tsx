607182921a722b4f93167dabee7a504fea387bbc
OlegAnisimov <gramafon@yandex.ru>
2023-06-05 12:52:41 +0300

    fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
 import './App.css';
+import axios from "axios";
+
+
+const fetchData = () => {
+  return axios
+    .get("https://randomuser.me/api/?results=20")
+    .then((res) => {
+      const { results } = res.data;
+      return results;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
 
 function App() {
   return (
